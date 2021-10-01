ac93543a8cc096bc9b1ff12e5ddc03450f7d56f4
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-10-01 00:43:03 -0300

    Update preload.js

--- test/preload.js
+++ test/preload.js
@@ -1,5 +1,7 @@
 const { insertFrame } = require('../npm/renderer')
 
 window.addEventListener('DOMContentLoaded', () => {
-    insertFrame({ darkMode: true })
+    const icon = new Image()
+    icon.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/209/waving-hand-sign_1f44b.png"
+    insertFrame({ icon })
 })
