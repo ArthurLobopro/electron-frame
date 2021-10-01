a389cb9e57c3ff3db6bacd5f0da568e06f87ec20
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-09-30 22:44:24 -0300

    fix old versions problem

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -2,7 +2,7 @@ import { ipcRenderer } from 'electron'
 import { makeFrame } from './frame'
 import { injectCSS } from './Util'
 
-async function insertFrame({darkMode = true}) {
+async function insertFrame({darkMode = true} = {}) {
     const frame = await makeFrame({darkMode, ...ipcRenderer.sendSync('request-window-config')})
     injectCSS(__dirname, 'style.css')
     document.body.appendChild(frame)
