23c4b8571c477b9c353d45ae3ac3b084eb735c95
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-10-05 17:57:11 -0300

    add color custom

--- src/renderer/frame.ts
+++ src/renderer/frame.ts
@@ -4,28 +4,49 @@ import { minimize, expand, close } from './renderer-actions'
 
 const assetsFolder = path.resolve(__dirname, "assets")
 
-interface makeFrame {
+interface makeFrameOptions {
     darkMode?: boolean
     title?: string
-    icon: HTMLImageElement | string
+    icon?: HTMLImageElement | string
     minimizable: boolean
     maximizable: boolean
     closeable: boolean
+    colors?: {
+        background?: string
+        color?: string
+        svgIconsColor?: string
+        svgIconsColorHover?: string
+        lastSvgIconHover?: string
+    }
 }
 
-async function makeFrame({ darkMode = true, title, icon, minimizable = true, maximizable = true, closeable = true }: makeFrame) {
+const format = (str: string) => str.replaceAll(/([A-Z])/g, s => `-${s.toLowerCase()}`)
+
+async function makeFrame(frameOptions: makeFrameOptions) {
+    const {
+        title, icon,
+        darkMode = true, minimizable = true, maximizable = true, closeable = true,
+        colors = {}
+    } = frameOptions
+
+    const colorsArray = Object.entries(colors)
+    const properties = colorsArray.map(([key, value]) => `--${format(key)} : ${value} !important`).join(';')
+   
+
     const windowIconString = icon || await getIconString()
 
     const frame = document.createElement('div')
     const name = title || document.title
 
     frame.className = darkMode ? "dark" : "light"
+    if(colorsArray.length !== 0){
+        frame.classList.add('custom')
+    }
 
     frame.id = "electron-frame"
     frame.innerHTML = `
     <div class="left">
-        <div id="window-icon">${
-            windowIconString instanceof Image ? windowIconString.outerHTML : windowIconString
+        <div id="window-icon">${windowIconString instanceof Image ? windowIconString.outerHTML : windowIconString
         }</div>
         <div id="window-name">${name}</div>
     </div>
@@ -39,7 +60,13 @@ async function makeFrame({ darkMode = true, title, icon, minimizable = true, max
         <div id="close" class="${closeable ? "" : "disable"}">
             ${loadSVG(assetsFolder, 'close.svg')}
         </div>
-    </div>`
+    </div>
+    <style>
+        #electron-frame.custom {
+            ${properties}
+        }
+    </style>
+    `
 
     const frameGet = (id: string) => frame.querySelector(`#${id}`) as HTMLElement
 
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,14 +1,20 @@
 import { ipcRenderer } from 'electron'
 import { makeFrame } from './frame'
 import { injectCSS } from './Util'
-
-interface insertFrameArgs{
+interface insertFrameOptions {
     darkMode?: boolean
-    icon?: string | HTMLImageElement
     title?: string
+    icon?: HTMLImageElement | string
+    colors?: {
+        background?: string
+        color?: string
+        svgIconsColor?: string
+        svgIconsColorHover?: string
+        lastSvgIconHover?: string
+    }
 }
 
-async function insertFrame( options: insertFrameArgs = {}) {
+async function insertFrame( options: insertFrameOptions = {}) {
     const frame = await makeFrame({...options, ...ipcRenderer.sendSync('request-window-config')})
     injectCSS(__dirname, 'style.css')
     document.body.appendChild(frame)
