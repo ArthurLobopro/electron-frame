0968079d1495adb9a35c7a5512a214c5386c48ec
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-10-11 12:46:51 -0300

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,34 +1,28 @@
 import { ipcMain, BrowserWindow } from 'electron'
 
-const mainWindowControlEvents = {
-    init() {
-        ipcMain.on('minimize', () => {
-            const win = BrowserWindow.getFocusedWindow()
-            win?.minimize()
-        })
+ipcMain.on('minimize', () => {
+    const win = BrowserWindow.getFocusedWindow()
+    win?.minimize()
+})
 
-        ipcMain.on('expand', () => {
-            const win = BrowserWindow.getFocusedWindow()
-            if (win?.isMaximized()) {
-                win?.restore()
-            } else {
-                win?.maximize()
-            }
-        })
-
-        ipcMain.on('close', () => {
-            const win = BrowserWindow.getFocusedWindow()
-            win?.close()
-        })
-
-        ipcMain.on('request-window-config', (event) => {
-            const win = BrowserWindow.getFocusedWindow()
-            const minimizable = win?.isMinimizable()
-            const maximizable = win?.isMaximizable()
-            const closeable = win?.isClosable()
-            event.returnValue = { minimizable, maximizable, closeable }
-        })
+ipcMain.on('expand', () => {
+    const win = BrowserWindow.getFocusedWindow()
+    if (win?.isMaximized()) {
+        win?.restore()
+    } else {
+        win?.maximize()
     }
-}
+})
 
-export { mainWindowControlEvents }
+ipcMain.on('close', () => {
+    const win = BrowserWindow.getFocusedWindow()
+    win?.close()
+})
+
+ipcMain.on('request-window-config', (event) => {
+    const win = BrowserWindow.getFocusedWindow()
+    const minimizable = win?.isMinimizable()
+    const maximizable = win?.isMaximizable()
+    const closeable = win?.isClosable()
+    event.returnValue = { minimizable, maximizable, closeable }
+})
