8e083b1ee9992bf2cd369790ba185113f3356035
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-10-11 20:07:54 -0300

    update test preload

--- test/preload.js
+++ test/preload.js
@@ -3,5 +3,11 @@ const { insertFrame } = require('../npm/renderer')
 window.addEventListener('DOMContentLoaded', () => {
     const icon = new Image()
     icon.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/209/waving-hand-sign_1f44b.png"
-    insertFrame({ icon })
+    insertFrame({
+        icon,
+        onClose: {
+            beforeCallback: () => Math.random() > 0.8
+            //If the condition is true, the window will close
+        }
+    })
 })
