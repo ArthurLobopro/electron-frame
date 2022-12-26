f5fbb27eaca8de093876f298d050ebd85b62d324
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-26 13:02:02 -0300

    refactor: some refactors in test aplication

--- test/preload.js
+++ test/preload.js
@@ -1,9 +1,5 @@
 const { contextBridge } = require('electron')
-const { insertFrame, ElectronFrame, PopUpFrame, removeFrame } = require('electron-frame/renderer')
-
-// const frame = new ElectronFrame({
-//     frameStyle: "macos"
-// })
+const { ElectronFrame, PopUpFrame } = require('electron-frame/renderer')
 
 let frameStyle = "windows"
 let frameType = "electron"
@@ -57,14 +53,7 @@ contextBridge.exposeInMainWorld('debug', debug)
 window.addEventListener('DOMContentLoaded', () => {
     const icon = new Image()
     icon.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/209/waving-hand-sign_1f44b.png"
-    // insertFrame({
-    //     icon,
-    //     onClose: {
-    //         // beforeCallback: () => Math.random() > 0.8
-    //         //If the condition is true, the window will close
-    //     }
-    // })
-    // frame.setIcon(icon)
+
     frame.insert()
 
     const selec_style = document.getElementById("style")
